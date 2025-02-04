f5ec74b7356914cd83db8c8c4e5bddc92e12fd56
heatherlyrobert <jelloshrike@gmail.com>
2025-02-04 05:52:50 -0800

    2.2c : yJOBS_maint unit tested audit and fix with reasonable files

--- khronos.h
+++ khronos.h
@@ -36,8 +36,8 @@
 /*········· ··········· ´·····························´········································*/
 #define     P_VERMAJOR  "2.--, simplify and harden code"
 #define     P_VERMINOR  "2.2-, yJOB_maint unit testing"
-#define     P_VERNUM    "2.2b"
-#define     P_VERTXT    "yJOBS_maint unit testing audit and fix in empty directory"
+#define     P_VERNUM    "2.2c"
+#define     P_VERTXT    "yJOBS_maint unit tested audit and fix with reasonable files"
 /*········· ··········· ´·····························´········································*/
 #define     P_TOPOFMIND "wild ideas, big experimental code base, single maintainer"
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
